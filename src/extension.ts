6eeeafc3ea7648ec73c2ad66e7ee086353f7c80a
maanijou <19888963+maanijou@users.noreply.github.com>
2021-07-30 19:10:28 +0200

    Add search command

--- src/extension.ts
+++ src/extension.ts
@@ -18,8 +18,30 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('ecosia-search.search', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello from Ecosia search');
+		let activeTextEditor = vscode.window.activeTextEditor;
+		if (!activeTextEditor) {
+			vscode.window.showInformationMessage('Make sure to activate an editor window first and select some texts.');
+			return;
+		}
+		// what about multiple selections? maybe a quick message
+		if (activeTextEditor.selection.isEmpty) {
+			vscode.window.showInformationMessage('Make sure to activate an editor window first and select some texts.');
+			return;
+		}
 
+		// TODO: Fix characters and whitespaces
+		let query: string = activeTextEditor.document.getText(activeTextEditor.selection);
+		if (!query) {
+			vscode.window.showInformationMessage('Make sure to activate an editor window first and select some texts.');
+			return;
+		}
+		query = encodeURI(query);
+		const url: string = `https://ecosia.org/search?q=${query}`;
+		if (url.length > MAX_URL_LENGHT) {
+			vscode.window.showInformationMessage('Try to select a smaller range of text. The selected text is too large.');
+			return;
+		}
+		vscode.commands.executeCommand("vscode.open", vscode.Uri.parse(url));
 	});
 	context.subscriptions.push(disposable);
 }
