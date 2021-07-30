eb7573cb460fdbc045f7079c8f89e039ead61df3
maanijou <19888963+maanijou@users.noreply.github.com>
2021-07-31 00:07:31 +0200

    Clean unnecessary comments

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,18 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
 const MAX_URL_LENGHT = 2047;
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('"ecosia-search" extension is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('ecosia-search.search', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
 		let activeTextEditor = vscode.window.activeTextEditor;
 		if (!activeTextEditor) {
 			vscode.window.showInformationMessage('Make sure to activate an editor window first and select some texts.');
 			return;
 		}
-		// what about multiple selections? maybe a quick message
+
 		if (activeTextEditor.selection.isEmpty) {
 			vscode.window.showInformationMessage('Make sure to activate an editor window first and select some texts.');
 			return;
@@ -46,5 +35,4 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() { }
