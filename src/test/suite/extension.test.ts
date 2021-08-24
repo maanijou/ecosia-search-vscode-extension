ac22da0b20f63d218e56ed23023b6c86e09c8420
maanijou <19888963+maanijou@users.noreply.github.com>
2021-08-25 00:02:06 +0200

    Add test for appending languageID

---
+++ src/test/suite/extension.test.ts
@@ -0,0 +1,15 @@
+import * as assert from 'assert';
+import * as vscode from 'vscode';
+import { appendLanguageID } from "../../utils/utils";
+
+suite('Extension Test Suite', () => {
+	vscode.window.showInformationMessage('Start all tests.');
+
+	test('Test appending language id', () => {
+		let expect: string = 'Some text +python';
+		let query: string = "Some text";
+		assert(expect === appendLanguageID(query, "python", "ecosia"));
+		assert(expect === appendLanguageID(query, "python", "duckduckgo"));
+		assert(expect === appendLanguageID(query, "python", "google"));
+	});
+});
